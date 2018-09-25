0cd3ba61eaffcbc82dc1af3f50e38287094fd0f2
Alireza Andersen <alireza.andersen@gmail.com>
2018-09-25 13:36:29 -0600

    renamed method

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -9,7 +9,7 @@ class Medication < ApplicationRecord
   end
 
   #groups diseases as key and all drugs related to it. {(disease.id)96929=>{(drug.id)1312=>(number of times prescribed to treat drugs)105, 8715=>85, 5764=>196, 3360=>201, 3940=>140}
-  def self.group_by_disease
+  def self.group_drug_by_disease
     disease_hash = Hash.new(0)
     self.most_med_per_disease.each do |dis, arr |
       disease = Disease.find(dis).id
@@ -26,7 +26,7 @@ class Medication < ApplicationRecord
   #{"Disease: disease name "=>"Drug: drug name"}
   def self.most_common_medication_ordered_to_treat_each_disease
     hospital_drugs = Hash.new()
-    self.group_by_disease.each do |hos_id, med_hash|
+    self.group_drug_by_disease.each do |hos_id, med_hash|
       popular_meds = med_hash.max_by { |k, v| v }
       drug = Drug.find(popular_meds[0]).name
       disease = Disease.find(hos_id).name
